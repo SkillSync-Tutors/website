2fcfba6834b73699fe0e2d0cc74d0fd7d9afa62c
StanleyChen50 <stanleychen50@gmail.com>
2024-06-18 22:32:46 -0400

    Changed company name on the navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex flex-wrap justify-between items-center">
-        <div className="text-white text-xl font-bold">BrandName</div>
+        <div className="text-white text-xl font-bold">SkillSync</div>
         <div className="flex flex-grow justify-end">
           <a href="#home" className="text-white hover:text-gray-300 mx-2">
             Home
           </a>
           <a href="#about" className="text-white hover:text-gray-300 mx-2">
-            About
+            About Us
           </a>
           <a href="#services" className="text-white hover:text-gray-300 mx-2">
             Services
