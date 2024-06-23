c98bbd281d94415eae299f7a422a883a065fb544
edwarddgao <edwarddgao@gmail.com>
2024-06-23 19:38:54 -0400

    Center links, add sign in and company icon

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,8 +4,11 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex flex-wrap justify-between items-center">
-        <div className="text-white text-xl font-bold">SkillSync</div>
-        <div className="flex flex-grow justify-end">
+        <div className="flex items-center">
+          <img src="/company-icon.svg" alt="Company Icon" className="w-10 h-10" />
+          <div className="text-white text-xl font-bold">SkillSync</div>
+        </div>
+        <div className="flex flex-grow justify-center">
           <a href="#home" className="text-white hover:text-gray-300 mx-2">
             Home
           </a>
@@ -19,6 +22,9 @@ const Navbar = () => {
             Contact
           </a>
         </div>
+        <a href="#signin" className="text-white hover:text-gray-300 font-bold">
+          Sign In
+        </a>
       </div>
     </nav>
   );
