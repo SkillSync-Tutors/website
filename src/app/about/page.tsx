48a6c6f4272fdb4a1db487e809afbebbb40fdfda
Tai Li <sphorq@gmail.com>
2024-06-28 02:15:38 -0400

    completed about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,48 +1,174 @@
-const list = ['1', '2', '3', '4', '5', '6', '1', '2', '3',]
 
 export default async function About() {
   return (
-    <div className="h-auto w-auto text-lg bg-[#011E32]">
+    <div className="h-auto w-auto text-lg bg-[#F5F5F5]">
       
-      About Page
-      <div className="w-auto h-auto flex justify-center items-center mx-40 my-40 space-x-60">
-        <div className="w-1/4 border-d9d9d9 border-2 h-auto bg-[#d9d9d9] rounded-md">
-          <p className="px-4 py-3">
-            mission statement
+      <div className="w-auto h-auto flex justify-center items-center mx-40 mt-40 lg:space-x-50 md:space-x-40 sm:space-x-20">
+        <div className="w-1/2 border-d9d9d9 h-auto">
+          <h1 className="text-[#28707B] text-7xl">
+            Who are we
+          </h1>
+          <p className="pr-4 py-3 text-[#28707B]">
+            Hey there! We're SkillSkync Tutors, born from a passion to help students succeed. Our journey started with a simple goal: to provide personalized, high-quality tutoring to Ontario's Grade 11-12 students. Our team is a mix of educators and enthusiasts who believe in making learning engaging and effective.
+          </p>
+          <p className="pr-4 py-3 text-[#28707B]">
+            Our tutors aren't just experts; they're mentors who inspire and guide students to achieve their best. 
+            We've been recognized for our innovative approach to education, and we're proud of the success stories we've helped create.
+            Every tutoring session is crafted to meet your unique needs, ensuring you get the support you need to excel. 
+            We believe in the power of education to change lives, and we're here to help you reach your academic dreams.
+          </p>
+          <p className="pr-4 py-3 text-[#28707B]">
+            Join us, and let's make your learning journey remarkable!
           </p>
         </div>
 
-        <div className="flex justify-center">
-          <img src="/emptyrectangle.png"  alt="Rectangle" className="rounded-md w-200"></img>
+        <div className="flex justify-center items-center">
+          <img src="./about/zxc5342532354.png"  alt="eric" className="rounded-md lg:h-auto lg:w-1/2 md:h-auto md:w-60 sm:h-auto sm:w-20 translate-x-20"></img>
         </div>
       </div>
 
+      <div className="bg-[#ECF2F3] w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20">
+        <div className="text-[#28707B] text-7xl mb-10 mt-20 mx-20 flex justify-center pt-20">
+          Our Team
+        </div>
+        <div className="flex justify-center items-center space-x-40">
+          <div className="w-1/2 mr-60">
+            <h1 className="text-[#28707B] text-5xl -translate-x-20 flex flex-row font-bold">
+              Eric Jun 
+              <p className="font-normal translate-x-5">
+               - CEO
+              </p>
+            </h1>
+            <p className="-translate-x-20">
+              Eric Jun Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun 
+              Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun Small 
+              BIOEric Jun Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun 
+              Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun Small BIOEric Jun Small BIOEric 
+              Jun Small BIOEric Jun Small BIOEric Jun Small BIO
+            </p>
+          </div>
+          <img src="./about/erichappy.png" alt="when eric was happy png" className="rounded-md w-1/4 -translate-x-40 mb-10"></img>
+        </div>
+      </div>
+ 
+      <div className="w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-center">
+        <img src="./about/bukyjoetiny.png" alt="small buky joe you don't need to see this" className="rounded-md w-1/4 mt-20 scale-75"></img>
+        <div className="w-3/5">
+        <h1 className="text-[#28707B] text-5xl flex flex-row font-bold">
+              Alvin Cheng 
+              <p className="font-normal translate-x-5">
+               - Vice President
+              </p>
+            </h1>
+            <p className="w-4/5">
+                BUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE 
+                SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE
+               SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE 
+               SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIOBUKYJOE SMALL 
+               BIOBUKYJOE SMALL BIOBUKYJOE SMALL BIO  
+            </p>
+        </div>
+      </div>
 
-      <div className="text-white flex justify-center items-center my-10 mx-20 p-10">
-        Our Team
+      <div className="bg-[#ECF2F3] w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-start">
+        <div className="flex flex-col items-center justify-center w-1/3">
+          <img src="./about/erickim.png" alt="eric kim" className="rounded-md mt-20 h-96"></img>
+          <h1 className="text-[#28707B]">
+            Eric Kim - Project Manager
+          </h1>
+          <p className="text-sm">
+            Eric Kim iS Erick MkIM iS eric kImI SISS KIM KIM IS ERICEric Kim iS Erick 
+            MkIM iS eric kImI SISS KIM KIM IS ERICEric Kim iS Erick MkIM iS eric kImI SISS 
+            KIM KIM IS ERICEric Kim iS Erick MkIM iS eric kImI SISS KIM KIM IS ERIC
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-1/3">
+          <img src="./about/dondo.png" alt="Don Do" className="rounded-md mt-20 h-96 "></img>
+          <h1 className="text-[#28707B]">
+            Don Do - Marketing Lead
+          </h1>
+          <p className="text-sm">
+            DON IS dond oN isdn oNNN ON the don THE DONNNNDON IS dond oN isdn oNNN ON 
+            the don THE DONNNNDON IS dond oN isdn oNNN ON the don THE DONNNNDON IS dond oN 
+            isdn oNNN ON the don THE DONNNN
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-1/3">
+          <img src="./about/joshma.png" alt="Joshua Ma" className="rounded-md mt-20 h-96"></img>
+          <h1 className="text-[#28707B]">
+            Joshua Ma - Techinal Lead
+          </h1>
+          <p className="text-sm pb-10">
+            jlsajfjfjlsjajljlsfjfsjsflajljasljsfjfjlsaljasljljfjlfjlsaljasjl ISJSLJFALJASLJASFLJFSLAJFSLJALJSALJjlsajfjfj 
+            lsjajljlsfjfsjsflajlja
+            sljsfjfjlsaljasljljfjlfjlsaljasjl IS JSLJFALJAS LJASFLJFSLAJFSLJALJSALJjlsajfjfjlsjaj 
+            ljlsfjfsjsflajljasljsfjfjlsaljasljljfjlfjlsaljasjl IS JSLJFALJASLJASFLJFSLAJFSLJALJSALJ
+          </p>
+        </div>
+      </div>
+
+      <div className=" w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-start">
+        <div className="flex flex-col items-center justify-center w-1/4">
+          <img src="./about/edwardgao.png" alt="edward gao" className="rounded-md mt-20 h-64"></img>
+          <h1 className="text-[#28707B]">
+            Edward Gao
+          </h1>
+          <p className="text-sm">
+            EDWARD GAO MISTAKE MISTAEK MISTAEK MIASTAEKFIJEWOIAFJOAIJFEDWARD GAO 
+            MISTAKE MISTAEK MISTAEK MIASTAEKFIJEWOIAFJOAIJFEDWARD GAO MISTAKE MISTAEK 
+            MISTAEK MIASTAEKFIJEWOIAFJOAIJFEDWARD GAO
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-1/4">
+          <img src="./about/taili.png" alt="Tai Li" className="rounded-md mt-20 h-64"></img>
+          <h1 className="text-[#28707B]">
+            Tai Li
+          </h1>
+          <p className="text-sm">
+            TAI LI SPHOR sphor sphoqqqqqq sphorq SPHORQ sph tai li TAI LI SPHOR sphor 
+            sphoqqqqqq sphorq SPHORQ sph tai li TAI LI SPHOR sphor sphoqqqqqq sphorq SPHORQ sph tai li 
+            TAI LI SPHOR sphor sphoqqqqqq sphorq SPHORQ sph tai li </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-1/4">
+          <img src="./about/stan.png" alt="Stanley Chen" className="rounded-md mt-20 h-64"></img>
+          <h1 className="text-[#28707B]">
+            Stanley Chen
+          </h1>
+          <p className="text-sm">
+            STAN LEE stanley for why why sSTLAN sltjoia iojfoeiw jiof?STAN LEE stanley for why why 
+            sSTLAN sltjoia iojfoeiw jiof?STAN LEE stanley for why why sSTLAN sltjoia iojfoeiw jiof?STAN 
+            LEE stanley for why why sSTLAN sltjoia iojfoeiw jiof?
+          </p>
+        </div>
+
+        <div className="flex flex-col items-center justify-center w-1/4">
+          <img src="./about/joshma.png" alt="Joseph Wang" className="rounded-md mt-20 h-64 opacity-0"></img>
+          <h1 className="text-[#28707B]">
+            Joseph Wang
+          </h1>
+          <p className="text-sm pb-20">
+            QUANTUM POTATO JOSEPH WANGQUANTUM POTATO JOSEPH WANGQUANTUM POTATO JOSEPH 
+            WANGQUANTUM POTATO JOSEPH WANGQUANTUM POTATO JOSEPH WANGQUANTUM POTATO JOSEPH 
+            WANGQUANTUM POTATO JOSEPH WANG
+          </p>
+        </div>
       </div>
       
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 grid-flow-row row-span-4 ml-60 mr-60">
-        
-        {list.map((item)=>(
-          <div className="flex justify-center items-center flex-col m-5">
-          <div className="w-36 h-36 rounded-full border-2 bg-[#d9d9d9]">
-            <p className="text-center">
-              circle
-            </p>
+      <div className="bg-[#103C42] flex relative justify-center items-center">
+        <img src="./about/bgpalce.png" alt="Background" className="rounded-md my-20 w-5/6"></img>
+          <div className="absolute text-white text-5xl -translate-y-20">
+            Get Started
           </div>
-          <div className="flex justify-center items-center w-40 h-6 border-2 mt-3 bg-[#d9d9d9]"> 
-            <p className="text-center">
-              {item}
-            </p>
+          <a href="services" className="absolute translate-y-10" target="_blank">
+            <div className=" text-white text-2xl bg-[#103C42] p-5 rounded-full scale-75 hover:scale-[0.8] hover:brightness-[0.95] transition duration-150 ease-in-out">
+              View Services
+            </div>
+          </a>
           </div>
-        </div>
-        ))}
-        
-
-
-      </div>
-
     </div>
   );
 }
