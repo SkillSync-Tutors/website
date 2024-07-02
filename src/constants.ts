cecb78d941155b9070caa3bc16bb57bed889b168
Joseph Ma <josephma293@gmail.com>
2024-07-02 11:33:55 -0400

    setup tutor booking page

--- src/constants.ts
+++ src/constants.ts
@@ -3,36 +3,48 @@ export const tutorData = [
     name: 'Black Woman',
     image: '/tutors/black_woman.png',
     school: 'Waterloo Comp Sci Student',
+    about:
+      'Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)',
     teachables: ['Functions Grade 11 (MCR3U)', 'Functions Grade 12 (MCR4U)'],
   },
   {
     name: 'White Man',
     image: '/tutors/white_man.png',
     school: 'McMaster Health Science Student',
+    about:
+      'Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)',
     teachables: ['Biology Grade 12 (IDK4U)'],
   },
   {
     name: 'Black Woman',
     image: '/tutors/black_woman.png',
     school: 'Waterloo Comp Sci Student',
+    about:
+      'Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)',
     teachables: ['Functions Grade 11 (MCR3U)', 'Functions Grade 12 (MCR4U)'],
   },
   {
     name: 'White Man',
     image: '/tutors/white_man.png',
     school: 'McMaster Health Science Student',
+    about:
+      'Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)',
     teachables: ['Biology Grade 12 (IDK4U)'],
   },
   {
     name: 'Black Woman',
     image: '/tutors/black_woman.png',
     school: 'Waterloo Comp Sci Student',
+    about:
+      'Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)',
     teachables: ['Functions Grade 11 (MCR3U)', 'Functions Grade 12 (MCR4U)'],
   },
   {
     name: 'White Man',
     image: '/tutors/white_man.png',
     school: 'McMaster Health Science Student',
+    about:
+      'Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)Generic paragraph / short bio about self. (Hobbies, teaching experience, education, etc.)',
     teachables: ['Biology Grade 12 (IDK4U)'],
   },
 ];
