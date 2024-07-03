aefd27685f42cfb02cd48da7cdb67a3ebebb26dd
Tai Li <sphorq@gmail.com>
2024-07-02 20:30:18 -0400

    made changes based on comments

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,35 +5,35 @@ export default async function About() {
       
       <div className="w-auto h-auto flex justify-center items-center mx-40 mt-40 lg:space-x-50 md:space-x-40 sm:space-x-20">
         <div className="w-1/2 border-d9d9d9 h-auto">
-          <h1 className="text-[#28707B] text-7xl">
+          <h1 className="text-cyan-800 text-7xl">
             Who are we
           </h1>
-          <p className="pr-4 py-3 text-[#28707B]">
+          <p className="pr-4 py-3 text-cyan-800">
             Hey there! We're SkillSkync Tutors, born from a passion to help students succeed. Our journey started with a simple goal: to provide personalized, high-quality tutoring to Ontario's Grade 11-12 students. Our team is a mix of educators and enthusiasts who believe in making learning engaging and effective.
           </p>
-          <p className="pr-4 py-3 text-[#28707B]">
+          <p className="pr-4 py-3 text-cyan-800">
             Our tutors aren't just experts; they're mentors who inspire and guide students to achieve their best. 
             We've been recognized for our innovative approach to education, and we're proud of the success stories we've helped create.
             Every tutoring session is crafted to meet your unique needs, ensuring you get the support you need to excel. 
             We believe in the power of education to change lives, and we're here to help you reach your academic dreams.
           </p>
-          <p className="pr-4 py-3 text-[#28707B]">
+          <p className="pr-4 py-3 text-cyan-800">
             Join us, and let's make your learning journey remarkable!
           </p>
         </div>
 
         <div className="flex justify-center items-center">
-          <img src="./about/zxc5342532354.png"  alt="eric" className="rounded-md lg:h-auto lg:w-1/2 md:h-auto md:w-60 sm:h-auto sm:w-20 translate-x-20"></img>
+          <img src="/about/zxc5342532354.png"  alt="eric" className="rounded-md lg:h-auto lg:w-1/2 md:h-auto md:w-60 sm:h-auto sm:w-20 translate-x-20"></img>
         </div>
       </div>
 
-      <div className="bg-[#ECF2F3] w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20">
-        <div className="text-[#28707B] text-7xl mb-10 mt-20 mx-20 flex justify-center pt-20">
+      <div className="bg-gray-100 w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20">
+        <div className="text-cyan-800 text-7xl mb-10 mt-20 mx-20 flex justify-center pt-20">
           Our Team
         </div>
         <div className="flex justify-center items-center space-x-40">
           <div className="w-1/2 mr-60">
-            <h1 className="text-[#28707B] text-5xl -translate-x-20 flex flex-row font-bold">
+            <h1 className="text-cyan-800 text-5xl -translate-x-20 flex flex-row font-bold">
               Eric Jun 
               <p className="font-normal translate-x-5">
                - CEO
@@ -47,14 +47,14 @@ export default async function About() {
               Jun Small BIOEric Jun Small BIOEric Jun Small BIO
             </p>
           </div>
-          <img src="./about/erichappy.png" alt="when eric was happy png" className="rounded-md w-1/4 -translate-x-40 mb-10"></img>
+          <img src="/about/erichappy.png" alt="when eric was happy png" className="rounded-md w-1/4 -translate-x-40 mb-10"></img>
         </div>
       </div>
  
       <div className="w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-center">
-        <img src="./about/bukyjoetiny.png" alt="small buky joe you don't need to see this" className="rounded-md w-1/4 mt-20 scale-75"></img>
+        <img src="/about/bukyjoetiny.png" alt="small buky joe you don't need to see this" className="rounded-md w-1/4 mt-20 scale-75"></img>
         <div className="w-3/5">
-        <h1 className="text-[#28707B] text-5xl flex flex-row font-bold">
+        <h1 className="text-cyan-800 text-5xl flex flex-row font-bold">
               Alvin Cheng 
               <p className="font-normal translate-x-5">
                - Vice President
@@ -70,10 +70,10 @@ export default async function About() {
         </div>
       </div>
 
-      <div className="bg-[#ECF2F3] w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-start">
+      <div className="bg-gray-100 w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-start">
         <div className="flex flex-col items-center justify-center w-1/3">
-          <img src="./about/erickim.png" alt="eric kim" className="rounded-md mt-20 h-96"></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/erickim.png" alt="eric kim" className="rounded-md mt-20 h-96"></img>
+          <h1 className="text-cyan-800">
             Eric Kim - Project Manager
           </h1>
           <p className="text-sm">
@@ -84,8 +84,8 @@ export default async function About() {
         </div>
 
         <div className="flex flex-col items-center justify-center w-1/3">
-          <img src="./about/dondo.png" alt="Don Do" className="rounded-md mt-20 h-96 "></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/dondo.png" alt="Don Do" className="rounded-md mt-20 h-96 "></img>
+          <h1 className="text-cyan-800">
             Don Do - Marketing Lead
           </h1>
           <p className="text-sm">
@@ -96,8 +96,8 @@ export default async function About() {
         </div>
 
         <div className="flex flex-col items-center justify-center w-1/3">
-          <img src="./about/joshma.png" alt="Joshua Ma" className="rounded-md mt-20 h-96"></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/joshma.png" alt="Joshua Ma" className="rounded-md mt-20 h-96"></img>
+          <h1 className="text-cyan-800">
             Joshua Ma - Techinal Lead
           </h1>
           <p className="text-sm pb-10">
@@ -111,8 +111,8 @@ export default async function About() {
 
       <div className=" w-auto h-auto px-40 lg:space-x-50 md:space-x-40 sm:space-x-20 flex justify-center items-start">
         <div className="flex flex-col items-center justify-center w-1/4">
-          <img src="./about/edwardgao.png" alt="edward gao" className="rounded-md mt-20 h-64"></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/edwardgao.png" alt="edward gao" className="rounded-md mt-20 h-64"></img>
+          <h1 className="text-cyan-800">
             Edward Gao
           </h1>
           <p className="text-sm">
@@ -123,8 +123,8 @@ export default async function About() {
         </div>
 
         <div className="flex flex-col items-center justify-center w-1/4">
-          <img src="./about/taili.png" alt="Tai Li" className="rounded-md mt-20 h-64"></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/taili.png" alt="Tai Li" className="rounded-md mt-20 h-64"></img>
+          <h1 className="text-cyan-800">
             Tai Li
           </h1>
           <p className="text-sm">
@@ -134,8 +134,8 @@ export default async function About() {
         </div>
 
         <div className="flex flex-col items-center justify-center w-1/4">
-          <img src="./about/stan.png" alt="Stanley Chen" className="rounded-md mt-20 h-64"></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/stan.png" alt="Stanley Chen" className="rounded-md mt-20 h-64"></img>
+          <h1 className="text-cyan-800">
             Stanley Chen
           </h1>
           <p className="text-sm">
@@ -146,8 +146,8 @@ export default async function About() {
         </div>
 
         <div className="flex flex-col items-center justify-center w-1/4">
-          <img src="./about/joshma.png" alt="Joseph Wang" className="rounded-md mt-20 h-64 opacity-0"></img>
-          <h1 className="text-[#28707B]">
+          <img src="/about/joshma.png" alt="Joseph Wang" className="rounded-md mt-20 h-64 opacity-0"></img>
+          <h1 className="text-cyan-800">
             Joseph Wang
           </h1>
           <p className="text-sm pb-20">
@@ -158,13 +158,13 @@ export default async function About() {
         </div>
       </div>
       
-      <div className="bg-[#103C42] flex relative justify-center items-center">
-        <img src="./about/bgpalce.png" alt="Background" className="rounded-md my-20 w-5/6"></img>
+      <div className="bg-cyan-950 flex relative justify-center items-center">
+        <img src="/about/bgpalce.png" alt="Background" className="rounded-md my-20 w-5/6"></img>
           <div className="absolute text-white text-5xl -translate-y-20">
             Get Started
           </div>
           <a href="services" className="absolute translate-y-10" target="_blank">
-            <div className=" text-white text-2xl bg-[#103C42] p-5 rounded-full scale-75 hover:scale-[0.8] hover:brightness-[0.95] transition duration-150 ease-in-out">
+            <div className=" text-white text-2xl bg-cyan-950 p-5 rounded-full scale-75 hover:scale-[0.8] hover:brightness-[0.95] transition duration-150 ease-in-out">
               View Services
             </div>
           </a>
