a845c8a7cdd5e38d1b584c16efbfeac107230006
Tai Li <sphorq@gmail.com>
2024-07-09 00:03:55 -0400

    second rendition of footer complete

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,37 +1,72 @@
 export default async function Footer() {
     return (
-    <>
-    <div className="flex bg-cyan-950 *:text-white">
-        <div className="p-16 pl-32">
-            <h1 className="text-5xl">SkillSync Tutors</h1>
-        </div>
-        <div className="flex flex-col space-y-6 pt-20 p-10 pl-56">
-            <h1>Explore</h1>
-            <div className="*:underline flex flex-col space-y-3">
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/about">About Us</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/contact">Contact</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/shop">Shop</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/subjects">Subjects</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/services">Services</a>
+    <div>
+        <div className="flex bg-[#103C42] *:text-white">
+            <div className="p-16 pl-32 flex flex-row">
+                <img src="/footer/logo.webp" alt="SkillSync Logo" className="w-24 h-24"></img>
+                <h1 className="text-5xl pt-6 pl-6">SkillSync Tutors</h1>
+            </div>
+            <div className="flex flex-col space-y-6 pt-20 p-10 pl-40">
+                <h1>Explore</h1>
+                <div className="*:underline flex flex-col space-y-3">
+                    <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/about">About Us</a>
+                    <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/contact">Contact</a>
+                    <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/shop">Shop</a>
+                    <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/subjects">Subjects</a>
+                    <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" href="/services">Services</a>
+                </div>
+            </div>
+            <div className="flex flex-col space-y-6 pt-20 p-10 pl-20">
+                <h1>Follow Us</h1>
+                <div className="flex flex-col space-y-3">
+                    <a className="underline hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline flex">
+                        <img className="w-5 h-5 mr-3" src="/footer/email.png"></img>
+                        Email
+                    </a>
+                        
+                    <a className="underline hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline flex">
+                        <img className="w-5 h-5 mr-3" src="/footer/fb.png"></img>
+                        Facebook
+                    </a>
+                        
+                    <a className="underline hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline flex">
+                        <img className="w-5 h-5 mr-3" src="/footer/ig.png"></img>
+                        Instagram
+                    </a>
+                        
+                    <a className="underline hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline flex">
+                        <img className="w-5 h-5 mr-3" src="/footer/linkedin.png"></img>
+                        Linked-In
+                    </a>
+
+                    <a className="underline hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline flex">
+                        <img className="w-5 h-5 mr-3" src="/footer/tt.png"></img>
+                        TikTok
+                    </a>
+                </div>
+            </div>
+            <div className="pl-20 *:text-center">
+                <div className="bg-[#005F65] m-10 flex flex-col items-center justify-center rounded-lg border-white border-4 shadow-lg">
+                    <h1 className="pt-12 px-10 pb-5">Stay Updated with Our Latest Events!</h1>
+                    <p className="pb-14">Subscribe to Our Newsletter for <br/>Exclusive News and Invitations.</p>
+                    <div className="relative group">
+                        <input placeholder="Email Address" className="p-3 rounded-lg text-black w-5/6 mb-5 group"></input>
+                        <button className="absolute text-black right-7 top-3 invisible group-focus-within:visible"><img src="/footer/rarrow.png" className="w-5 h-5"></img></button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div className="flex flex-col space-y-6 pt-20 p-10 pl-20">
-            <h1>Follow Us</h1>
-            <div className="*:underline flex flex-col space-y-3">
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" >Email</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" >Facebook</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" >Instagram</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" >Linked-In</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" >TikTok</a>
-                <a className="hover:text-gray-500 transition duration-150 ease-in-out hover:no-underline" >X</a>
-            </div>
-        </div>
-        <div className="pl-20">
-            <div className="bg-cyan-800 pl-10 pt-28 pb-44 pr-36 m-10 flex">
-                <input placeholder="Email Address" className="p-3 rounded-lg text-black"></input>
-            </div>
+
+        <div className="bg-[#005F65] border-white border-t-2">
+            <p className="text-white p-12">
+                © 2024 <a href="/" className="hover:underline transition duration-150 ease-in-out">SkillSync Tutors Inc.</a> All rights reserved. <br/><br/> 
+
+                The SkillSync Tutors logo and name are trademarks of SkillSync Tutors Inc. 
+                Unauthorized use and/or duplication of this material without express and written 
+                permission from SkillSync Tutors Inc. is strictly prohibited. <br/> All other trademarks, 
+                logos, and copyrights are the property of their respective owners.
+            </p>
         </div>
     </div>
-    </>
     );
 }
